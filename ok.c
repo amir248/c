4c5991cb247dd06d0496966c63f6b7ab7610da91
chikchicly <chikchicly@gmail.com>
2022-04-15 19:43:05 +0700

    ok

--- ok.c
+++ ok.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main(){
 	int Count;
-	for(Count=0;Count<10;Count++){
+	for(Count=0;Count<12;Count++){
 		printf("%u \n" , Count);
-	}		
+	}
 	printf("Amir Shikaren! :-) Yra!!!\n");
 	printf("Guten ");
 	printf("\n ok, еще позитивное мышление!\n");
 }
-
