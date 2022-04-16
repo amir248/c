715d8dd1492cc603d7b929aab7a2e694043ca9dd
amir248 <chikchicly@gmail.com>
2022-04-16 15:40:35 +0700

    new string

--- ok.c
+++ ok.c
@@ -4,7 +4,7 @@ int main(){
 	for(Count=0;Count<12;Count++){
 		printf("%u \n" , Count);
 	}
-	printf("Amir Shikaren! :-) Yra!!!\n");
+	printf("Amir Shikaren! :-) Yra!!!\n and all gut exellent\n today is a great day\n");
 	printf("Guten ");
 	printf("\n ok, еще позитивное мышление!\n");
 }
