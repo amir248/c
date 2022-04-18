e86d156e2e98c78e51a11a7163401e3347d7f304
ChikChicly <chikchicly@gmail.com>
2022-04-19 06:40:53 +0700

    two file

---
+++ two.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int Count;
+	for(Count=0;Count<10;Count++){
+		printf("%D%\n" , Count);
+	}
+	
+	printf("Super GitHub and programming in C\n git hub and C programming\n");	
+	printf("Amir Shikaren! :-) Yra!!!\n");
+	printf("Guten ");
+	printf("\n ok, еще позитивное мышление!\n");
+}
+
