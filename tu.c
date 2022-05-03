95f9f85c5dd353c45ee3938c2afdd6b845c1ea24
ChikChicly <chikchicly@gmail.com>
2022-05-04 06:17:37 +0700

    tuLuLa tuTu

--- tu.c
+++ tu.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 main(){
 	int integer;
-	scanf("%d", &integer);
-	printf("%d", &integer);
+	char name[7];
+	printf("Как тебя зовут?");
+	scanf("%s", &name);
+	printf("hello! " );
+	printf("%s", &name, "\n");
+
+	if(("%s", &name)=="Amir"){
+		printf("\n Yra!!! \n");
+	}else if(("%s",&name)!=1){
+		printf("\n ok \n");
+	}else{
+		printf("else \n");
+	}	
 }
